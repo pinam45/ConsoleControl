c7ea249f167376875a1d424d09cb8bd21d800821
Maxime Pinard <maxime.pin@live.fr>
2017-07-21 21:47:57 +0200

    CMake compilation files update for C project

--- cmake/gcc-settings.cmake
+++ cmake/gcc-settings.cmake
@@ -4,7 +4,7 @@ if(CMAKE_C_COMPILER_VERSION VERSION_LESS GCC_EXPECTED_VERSION)
 	message(FATAL_ERROR "GCC: Project requires version ${GCC_EXPECTED_VERSION} to build but found ${CMAKE_C_COMPILER_VERSION}")
 endif()
 
-message(STATUS "Found GCC ${CMAKE_CXX_COMPILER_VERSION}")
+message(STATUS "Found GCC ${CMAKE_C_COMPILER_VERSION}")
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -pedantic -Wall -Wcast-align -Wcast-qual -Wconversion -Wdisabled-optimization -Wdouble-promotion -Wextra -Wfloat-equal -Wformat -Winit-self -Winvalid-pch -Wlogical-op -Wmain -Wmissing-declarations -Wmissing-include-dirs -Wpointer-arith -Wredundant-decls -Wshadow -Wswitch-default -Wswitch-enum -Wundef -Wuninitialized -Wunreachable-code -Wwrite-strings")
 message(STATUS "GCC: Enabled warnings")
